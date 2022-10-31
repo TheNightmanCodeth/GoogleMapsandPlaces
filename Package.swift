44e88eeb7639197644ce8778f6ac9adf67f5e6d6
Joe Diragi <joe.diragi@pilottravelcenters.com>
2022-10-31 13:56:39 -0400

    Migrate to remote binary targets

--- Package.swift
+++ Package.swift
@@ -22,10 +22,10 @@ let package = Package(
         .target(
             name: "GoogleMapsandPlaces",
             dependencies: []),
-        .binaryTarget(name: "GoogleMaps", path: "Resources/GoogleMaps.xcframework"),
-        .binaryTarget(name: "GoogleMapsCore", path: "Resources/GoogleMapsCore.xcframework"),
-        .binaryTarget(name: "GoogleMapsBase", path: "Resources/GoogleMapsBase.xcframework"),
-        .binaryTarget(name: "GooglePlaces", path: "Resources/GooglePlaces.xcframework"),
+        .binaryTarget(name: "GoogleMaps", url: "https://github.com/TheNightmanCodeth/GoogleMapsandPlaces/releases/download/5.2maps5.1places/GoogleMaps.xcframework.zip", checksum: "eb548490f00ffb4c007c9ddebc717370b6d1907ed5c4910300593894a0b8f111"),
+        .binaryTarget(name: "GoogleMapsCore", url: "https://github.com/TheNightmanCodeth/GoogleMapsandPlaces/releases/download/5.2maps5.1places/GoogleMapsCore.xcframework.zip", checksum: "ddea2d79e508ebf6feeb20c0e4ff435f75eb732a3424dd3df9c5afa051e95824"),
+        .binaryTarget(name: "GoogleMapsBase", url: "https://github.com/TheNightmanCodeth/GoogleMapsandPlaces/releases/download/5.2maps5.1places/GoogleMapsBase.xcframework.zip", checksum: "deefe298622730297eb157cbf1000cb3ed534baa7c6e7ffede13a05841ccbf7f"),
+        .binaryTarget(name: "GooglePlaces", url: "https://github.com/TheNightmanCodeth/GoogleMapsandPlaces/releases/download/5.2maps5.1places/GooglePlaces.xcframework.zip", checksum: "9e4004378557cfdcfe9eaf8bc37884fc91886cfbe9acda2c708c64479def4b58"),
         .testTarget(
             name: "GoogleMapsandPlacesTests",
             dependencies: ["GoogleMapsandPlaces"]),
