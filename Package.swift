41871d30da51bb470e0e677446e2ffd63d88e751
Joe Diragi <joe.diragi@pilottravelcenters.com>
2022-11-04 15:49:17 -0400

    Version 7.1.0

--- Package.swift
+++ Package.swift
@@ -22,10 +22,10 @@ let package = Package(
         .target(
             name: "GoogleMapsandPlaces",
             dependencies: []),
-        .binaryTarget(name: "GoogleMaps", url: "https://github.com/TheNightmanCodeth/GoogleMapsandPlaces/releases/download/5.2maps5.1places/GoogleMaps.xcframework.zip", checksum: "eb548490f00ffb4c007c9ddebc717370b6d1907ed5c4910300593894a0b8f111"),
-        .binaryTarget(name: "GoogleMapsCore", url: "https://github.com/TheNightmanCodeth/GoogleMapsandPlaces/releases/download/5.2maps5.1places/GoogleMapsCore.xcframework.zip", checksum: "ddea2d79e508ebf6feeb20c0e4ff435f75eb732a3424dd3df9c5afa051e95824"),
-        .binaryTarget(name: "GoogleMapsBase", url: "https://github.com/TheNightmanCodeth/GoogleMapsandPlaces/releases/download/5.2maps5.1places/GoogleMapsBase.xcframework.zip", checksum: "deefe298622730297eb157cbf1000cb3ed534baa7c6e7ffede13a05841ccbf7f"),
-        .binaryTarget(name: "GooglePlaces", url: "https://github.com/TheNightmanCodeth/GoogleMapsandPlaces/releases/download/5.2maps5.1places/GooglePlaces.xcframework.zip", checksum: "9e4004378557cfdcfe9eaf8bc37884fc91886cfbe9acda2c708c64479def4b58"),
+        .binaryTarget(name: "GoogleMaps", url: "https://github.com/TheNightmanCodeth/GoogleMapsandPlaces/releases/download/maps7.1.0/GoogleMaps.xcframework.zip", checksum: "9e384665a05fa5fcc2354e5a4b2ee6a050b9e31fc1da46326fcd08fd685fcdbe"),
+        .binaryTarget(name: "GoogleMapsCore", url: "https://github.com/TheNightmanCodeth/GoogleMapsandPlaces/releases/download/maps7.1.0/GoogleMapsCore.xcframework.zip", checksum: "92e93d42b52ce874856876cc18f65094913c070ee4f27a068d4fba7a99fdda36"),
+        .binaryTarget(name: "GoogleMapsBase", url: "https://github.com/TheNightmanCodeth/GoogleMapsandPlaces/releases/download/maps7.1.0/GoogleMapsBase.xcframework.zip", checksum: "f112a0ba1e2d828fb6871c291a403113983cadbacd04cc2315379b2e0c9ed5a2"),
+        .binaryTarget(name: "GooglePlaces", url: "https://github.com/TheNightmanCodeth/GoogleMapsandPlaces/releases/download/maps7.1.0/GooglePlaces.xcframework.zip", checksum: "dc34b5824ca56ee839c76824d7617e54e9ef3ce2e423975649a4ae1e49fdbe7e"),
         .testTarget(
             name: "GoogleMapsandPlacesTests",
             dependencies: ["GoogleMapsandPlaces"]),
